5701c231414e06ff98ac80427bbce88459fb773a
Abdullah Azmy <abdullahazmy2@gmail.com>
2024-10-16 23:51:25 +0300

    Add project files.

---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,99 @@
+﻿namespace ConsoleApp1
+{
+    struct HiringDate : IComparable<HiringDate>
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Day}/{Month}/{Year}";
+        }
+
+        // Implementing IComparable to compare HireDates for sorting
+        public int CompareTo(HiringDate other)
+        {
+            if (this.Year != other.Year)
+                return this.Year.CompareTo(other.Year);
+            if (this.Month != other.Month)
+                return this.Month.CompareTo(other.Month);
+            return this.Day.CompareTo(other.Day);
+        }
+    }
+
+
+
+    internal class Employee
+    {
+        #region Basics
+        public int Id { get; set; }
+        public double Salary { get; set; }
+
+        // Property for HiringDate
+        public HiringDate HireDate { get; set; }
+
+        public string Gender { get; set; }
+        public static int Size { get; set; }
+
+        public Employee(int id = 0, double salary = 0, HiringDate? hireDate = null, string gender = "Unknown", int size = 0)
+        {
+            Id = id;
+            Salary = salary;
+            // If hireDate is null, set a default HiringDate
+            HireDate = hireDate ?? new HiringDate { Day = 1, Month = 1, Year = 1 };
+            Gender = gender;
+            Size = size;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Salary: {Salary}, HireDate: {HireDate}, Gender: {Gender}";
+        }
+
+        Employee[] employees = new Employee[Size];
+
+        #endregion
+
+        #region indexer
+        // Implement the indexer, for sorting Employees by HireDate
+
+        public Employee this[int index]
+        {
+            get
+            {
+                return employees[index];
+            }
+            set
+            {
+                employees[index] = value;
+            }
+        }
+
+        #endregion
+
+        #region Sorting Employees by HireDate
+        public void SortEmployeesByHireDate()
+        {
+            Array.Sort(employees, (x, y) => x.HireDate.CompareTo(y.HireDate));
+        }
+
+        #endregion
+
+
+        #region print the employees
+
+        public void print()
+        {
+            foreach (var emp in employees)
+            {
+                Console.WriteLine(emp.ToString());
+            }
+        }
+
+        #endregion
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Ask the user for the number of employees
+            Console.Write("Enter the number of employees: ");
+            int size = int.Parse(Console.ReadLine());
+
+            // Create an instance of Employee to manage the employees array
+            Employee company = new Employee(size: size); // Pass the size of the employees array
+
+            // Populate the employees array with user input
+            for (int i = 0; i < Employee.Size; i++)
+            {
+                Console.WriteLine($"Enter details for Employee {i + 1}:");
+
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                Console.Write("Hire Date (Day): ");
+                int day = int.Parse(Console.ReadLine());
+
+                Console.Write("Hire Date (Month): ");
+                int month = int.Parse(Console.ReadLine());
+
+                Console.Write("Hire Date (Year): ");
+                int year = int.Parse(Console.ReadLine());
+
+                HiringDate hireDate = new HiringDate { Day = day, Month = month, Year = year };
+
+                Console.Write("Gender: ");
+                string gender = Console.ReadLine();
+
+                // Add the employee to the array
+                company[i] = new Employee(id, salary, hireDate, gender);
+            }
+
+            // Sort the employees by hire date
+            company.SortEmployeesByHireDate();
+
+            // Print the sorted employees
+            Console.WriteLine("\nEmployees sorted by Hire Date:");
+            company.print();
+        }
+    }
+}
